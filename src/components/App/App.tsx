79e4957c9311e6def8d7df55b3020fc53b6c28de
Vladimir Artyukhov <ya.Artriches@ya.ru>
2020-11-11 11:55:25 +0300

    add: pages

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import Layout from '../../hoc/Layout';
+import Auth from '../../containers/Auth';
 import Quiz from '../../containers/Quiz';
+import QuizCreator from '../../containers/QuizCreator';
+import QuizList from '../../containers/QuizList';
 
 const App: React.FC = (): JSX.Element => {
   return (
     <Layout>
-      <Quiz />
+      <Switch>
+        <Route path="/auth" component={Auth} />
+        <Route path="/quiz-creator" component={QuizCreator} />
+        <Route path="/quiz/:id" component={Quiz} />
+        <Route path="/" component={QuizList} />
+      </Switch>
     </Layout>
   );
 };
---
+++ src/containers/Auth/Auth.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import styles from './auth.module.css';
+
+class Auth extends Component {
+  render() {
+    return (
+      <div>
+        <h1>auth</h1>
+      </div>
+    );
+  }
+}
+
+export default Auth;
