671313ee66aa26c55760f424937687d6c0a46d95
Vladimir Artyukhov <ya.Artriches@ya.ru>
2020-11-11 13:31:00 +0300

    add: auth page

--- src/containers/Auth/Auth.tsx
+++ src/containers/Auth/Auth.tsx
@@ -1,11 +1,33 @@
 import React, { Component } from 'react';
 import styles from './auth.module.css';
+import Button from '../../components/UI/Button';
 
 class Auth extends Component {
+  loginHandler = () => {};
+
+  registerHandler = () => {};
+
+  submitHandler = (evt: any) => {
+    evt.preventDefault();
+  };
+
   render() {
     return (
       <div className={styles.auth}>
-        <h1>auth</h1>
+        <div>
+          <h1>Авторизация</h1>
+          <form onSubmit={this.submitHandler} className={styles.authForm}>
+            <input type="text" />
+            <input type="text" />
+
+            <Button type="success" onClick={this.loginHandler}>
+              Войти
+            </Button>
+            <Button type="primary" onClick={this.registerHandler}>
+              Зарегистрироваться
+            </Button>
+          </form>
+        </div>
       </div>
     );
   }
