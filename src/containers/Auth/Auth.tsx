cc7d74e6095f2775d7d617c1e2d1587bcf4d5c2c
Vladimir Artyukhov <ya.Artriches@ya.ru>
2020-11-11 15:17:51 +0300

    feat: form validation

--- src/containers/Auth/Auth.tsx
+++ src/containers/Auth/Auth.tsx
@@ -3,28 +3,138 @@ import styles from './auth.module.css';
 import Button from '../../components/UI/Button';
 import Input from '../../components/UI/Input';
 
+function validateEmail(email: any): boolean {
+  const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
+
+interface IValidation {
+  required: boolean;
+  email?: boolean;
+  minLength?: number;
+}
+
 class Auth extends Component {
+  state: any = {
+    isFormValid: false,
+    formControls: {
+      email: {
+        value: '',
+        type: 'email',
+        label: 'Email',
+        errorMessage: 'Введите корректный email',
+        valid: false,
+        touched: false,
+        validation: {
+          required: true,
+          email: true,
+        },
+      },
+      password: {
+        value: '',
+        type: 'password',
+        label: 'Пароль',
+        errorMessage: 'Введите корректный пароль',
+        valid: false,
+        touched: false,
+        validation: {
+          required: true,
+          minLength: 6,
+        },
+      },
+    },
+  };
+
+  renderInputs = () => {
+    return Object.keys(this.state.formControls).map((controlName, index) => {
+      const control = this.state.formControls[controlName];
+      return (
+        <Input
+          key={controlName + index}
+          type={control.type}
+          value={control.value}
+          valid={control.valid}
+          touched={control.touched}
+          label={control.label}
+          errorMessage={control.errorMessage}
+          shouldValidate={!!control.validation}
+          onChange={(evt: any) => this.onChangeHandler(evt, controlName)}
+        />
+      );
+    });
+  };
+
+  validateControl = (value: string, validation: IValidation): boolean => {
+    if (!validation) {
+      return true;
+    }
+
+    let isValid = true;
+    if (validation.required) {
+      isValid = value.trim() !== '' && isValid;
+    }
+
+    if (validation.email) {
+      isValid = validateEmail(value) && isValid;
+    }
+
+    if (validation.minLength) {
+      isValid = value.trim().length >= validation.minLength && isValid;
+    }
+
+    return isValid;
+  };
+
+  onChangeHandler = (evt: any, controlName: string): void => {
+    const formControls = { ...this.state.formControls };
+    const control = { ...formControls[controlName] };
+
+    control.value = evt.target.value;
+    control.touched = true;
+    control.valid = this.validateControl(control.value, control.validation);
+
+    formControls[controlName] = control;
+
+    let isFormValid = true;
+
+    Object.keys(formControls).forEach((name) => {
+      isFormValid = formControls[name].valid && isFormValid;
+    });
+
+    this.setState({
+      isFormValid,
+      formControls,
+    });
+  };
+
   loginHandler = () => {};
 
   registerHandler = () => {};
 
-  submitHandler = (evt: any) => {
+  submitHandler = (evt: any): void => {
     evt.preventDefault();
   };
 
-  render() {
+  render(): JSX.Element {
     return (
       <div className={styles.auth}>
         <div>
           <h1>Авторизация</h1>
           <form onSubmit={this.submitHandler} className={styles.authForm}>
-            <Input label="Email" />
-            <Input label="Пароль" errorMessage={'TEST error'} />
+            {this.renderInputs()}
 
-            <Button type="success" onClick={this.loginHandler}>
+            <Button
+              type="success"
+              onClick={this.loginHandler}
+              disabled={!this.state.isFormValid}
+            >
               Войти
             </Button>
-            <Button type="primary" onClick={this.registerHandler}>
+            <Button
+              type="primary"
+              onClick={this.registerHandler}
+              disabled={!this.state.isFormValid}
+            >
               Зарегистрироваться
             </Button>
           </form>
--- src/containers/QuizCreator/QuizCreator.tsx
+++ src/containers/QuizCreator/QuizCreator.tsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import styles from './quizCreator.module.css';
 
 class QuizCreator extends Component {
-  render() {
+  render(): JSX.Element {
     return (
       <div className={styles.quizCreator}>
         <h1>quiz creator</h1>
