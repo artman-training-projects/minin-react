20e12ab6d021ca12ce164d5694d9bee66f837189
Vladimir Artyukhov <ya.Artriches@ya.ru>
2020-11-11 11:55:25 +0300

    add: pages

---
+++ src/containers/QuizCreator/QuizCreator.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import styles from './quizCreator.module.css';
+
+class QuizCreator extends Component {
+  render() {
+    return (
+      <div>
+        <h1>quiz creator</h1>
+      </div>
+    );
+  }
+}
+
+export default QuizCreator;
---
+++ src/containers/QuizList/QuizList.tsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import styles from './quizList.module.css';
+
+class QuizList extends Component {
+  render() {
+    return (
+      <div>
+        <h1>quiz list</h1>
+      </div>
+    );
+  }
+}
+
+export default QuizList;
