5a1bcfecc55d822a571157b80962b2cf6539e420
Vladimir Artyukhov <ya.Artriches@ya.ru>
2020-11-13 13:02:50 +0300

    feat: quiz in redux

--- src/store/actions/quiz.ts
+++ src/store/actions/quiz.ts
@@ -3,7 +3,13 @@ import {
   FETCH_QUIZ_ERROR,
   FETCH_QUIZ_START,
   FETCH_QUIZ_SUCCESS,
+  FETCH_QUIZIS_SUCCESS,
+  FINISH_QUIZ,
+  QUIZ_NEXT_QUESTION,
+  QUIZ_RETRY,
+  QUIZ_SET_STATE,
 } from './actionsTypes';
+import { IQuiz, IResults } from '../../types';
 
 export default function fetchQuiz() {
   return async (dispatch: any) => {
@@ -24,6 +30,111 @@ export default function fetchQuiz() {
     } catch (err) {
       dispatch(fetchQuizError(err));
     }
+  };
+}
+
+export function fetchQuizById(quizId: any) {
+  return async (dispatch: any) => {
+    dispatch(fetchQuizStart());
+
+    try {
+      const response = await axios.get(`/quizes/${quizId}.json`);
+
+      const quiz = response.data;
+
+      dispatch(fetchQuizisSuccess(quiz));
+    } catch (err) {
+      dispatch(fetchQuizError(err));
+    }
+  };
+}
+
+export function fetchQuizisSuccess(quizis: any) {
+  return {
+    type: FETCH_QUIZIS_SUCCESS,
+    quizis,
+  };
+}
+
+export function quizAnswerClick(answerId: any) {
+  return (dispatch: any, getState: any) => {
+    const state = getState().quiz;
+
+    if (state.answerState) {
+      const key = Object.keys(state.answerState)[0];
+      if (state.answerState[key] === 'success') {
+        return;
+      }
+    }
+
+    const question: IQuiz = state.quizis[state.activeQuestion];
+    const results: IResults = state.results;
+
+    if (question.rightAnswerId === answerId) {
+      if (!results[question.id]) {
+        results[question.id] = 'success';
+      }
+
+      dispatch(
+        quizSetState(
+          {
+            [answerId]: 'success',
+          },
+          results
+        )
+      );
+
+      const timeout: number = window.setTimeout(() => {
+        if (isQuizFinished(state)) {
+          dispatch(finishQuiz());
+        } else {
+          dispatch(quizNextQuestion(state.activeQuestion + 1));
+        }
+
+        window.clearTimeout(timeout);
+      }, 500);
+    } else {
+      results[question.id] = 'error';
+      dispatch(
+        quizSetState(
+          {
+            [answerId]: 'error',
+          },
+          results
+        )
+      );
+    }
+  };
+}
+
+export function quizSetState(answerState: any, results: any) {
+  return {
+    type: QUIZ_SET_STATE,
+    answerState,
+    results,
+  };
+}
+
+export function isQuizFinished(state: any) {
+  return state.activeQuestion + 1 === state.quizis.length;
+}
+
+export function finishQuiz() {
+  return {
+    type: FINISH_QUIZ,
+  };
+}
+
+export function retryQuiz() {
+  return {
+    type: QUIZ_RETRY,
+  };
+}
+
+export function quizNextQuestion(number: any) {
+  return {
+    type: QUIZ_NEXT_QUESTION,
+    number,
   };
 }
 
--- src/store/reducers/quiz.ts
+++ src/store/reducers/quiz.ts
@@ -2,12 +2,22 @@ import {
   FETCH_QUIZ_ERROR,
   FETCH_QUIZ_START,
   FETCH_QUIZ_SUCCESS,
+  FETCH_QUIZIS_SUCCESS,
+  FINISH_QUIZ,
+  QUIZ_NEXT_QUESTION,
+  QUIZ_RETRY,
+  QUIZ_SET_STATE,
 } from '../actions/actionsTypes';
 
 const initialState: any = {
   quiz: [],
   loading: false,
   error: null,
+  results: {},
+  isFinished: false,
+  activeQuestion: 0,
+  answerState: null,
+  quizis: null,
 };
 
 export default function quizReducer(state = initialState, action: any) {
@@ -28,6 +38,38 @@ export default function quizReducer(state = initialState, action: any) {
         ...state,
         loading: false,
         error: action.error,
+      };
+    case FETCH_QUIZIS_SUCCESS: {
+      return {
+        ...state,
+        loading: false,
+        quizis: action.quizis,
+      };
+    }
+    case QUIZ_SET_STATE:
+      return {
+        ...state,
+        answerState: action.answerState,
+        results: action.results,
+      };
+    case FINISH_QUIZ:
+      return {
+        ...state,
+        isFinished: true,
+      };
+    case QUIZ_NEXT_QUESTION:
+      return {
+        ...state,
+        answerState: null,
+        activeQuestion: action.number,
+      };
+    case QUIZ_RETRY:
+      return {
+        ...state,
+        activeQuestion: 0,
+        answerState: null,
+        isFinished: false,
+        results: {},
       };
     default:
       return state;
